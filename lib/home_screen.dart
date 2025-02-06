5c98ece26a59814a54d1071c7097ab79af485630
Milan Kapadiya <112311920+milankapadiya@users.noreply.github.com>
2025-02-06 12:14:55 +0530

    Initial commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 78, 151, 255),
+        title: const Center(child: Text('Myapp')),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+        ],
+        backgroundColor: const Color.fromARGB(255, 203, 211, 237),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:counterapp/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MycounterApp());
+}
+
+class MycounterApp extends StatelessWidget {
+  const MycounterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Homescreen());
+  }
+}
+
